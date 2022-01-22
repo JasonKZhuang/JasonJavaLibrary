b8c470ed1a8b8bf4f65d155bdd8d31aed7346493
Jason Zhuang <zhuangkaizhi@gmail.com>
2022-01-22 21:20:12 +1100

    update

--- src/main/java/com/jasonz/oop/test/Test.java
+++ src/main/java/com/jasonz/oop/test/Test.java
@@ -1,12 +1,20 @@
 package com.jasonz.oop.test;
 
+import com.jasonz.oop.utilities.IsNumberic;
+
 public class Test
 {
 
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-
+		String barcode = "00093499680050000001";
+		System.out.println(barcode.substring(0,2));
+		System.out.println(IsNumberic.isNumeric1(barcode.substring(11,barcode.length())));
+		barcode="";
+		if ( barcode.startsWith("CPAP") ||  barcode.startsWith("CPAY")){
+			System.out.println("fdsa");
+		}
 	}
 
 }
