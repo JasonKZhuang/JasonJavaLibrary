d3f9737476063f7e03e9e3678b67531b6e9d0636
Jason Zhuang <zhuangkaizhi@gmail.com>
2022-02-03 23:55:00 +1100

    update

--- src/main/java/com/jasonz/core/StringKits.java
+++ src/main/java/com/jasonz/core/StringKits.java
@@ -11,12 +11,23 @@ import java.util.stream.Stream;
  */
 public class StringKits {
     public static void main(String[] args) {
-        String v = "06104123";
+        StringKits kits = new StringKits();
+        String phoneNumber = "24123456789";
         List<String> lst = new ArrayList<>();
         lst.add("6104");
         lst.add("04");
         lst.add("4");
-        System.out.println(StringKits.usingStartsWith(v,lst));
+        System.out.println(StringKits.usingStartsWith(phoneNumber,lst));
+
+
+        //2. Check the prefix (country code) of the numbers to ensure it is an Australian mobile
+        List<String> prefixes = new ArrayList<String>();
+        prefixes.add("6104");
+        prefixes.add("04");
+        prefixes.add("4");
+        System.out.println( kits.convertAustraliaNumber(phoneNumber,prefixes));
+
+
     }
 
     public static String usingStartsWith(String arg, List<String> prefix) {
@@ -39,4 +50,27 @@ public class StringKits {
                 });
         return a.toString();
     }
+
+
+    private String convertAustraliaNumber(String phoneNumber, List<String> prefixes){
+        for (String v : prefixes) {
+            if (phoneNumber.startsWith("614")){
+                return phoneNumber;
+            }
+            if (phoneNumber.startsWith(v.trim())){
+                if(phoneNumber.startsWith("6104")){
+                    phoneNumber = "614".concat(phoneNumber.substring(4));
+                } else if(phoneNumber.startsWith("04")){
+                    phoneNumber = "614".concat(phoneNumber.substring(2));
+                }else if(phoneNumber.startsWith("4")){
+                    phoneNumber = "614".concat(phoneNumber.substring(1));
+                }else{
+                    phoneNumber = "";
+                }
+                return phoneNumber;
+            }
+        }
+        return "";
+    }
+
 }
